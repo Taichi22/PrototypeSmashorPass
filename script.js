dd827867e63b144f88bb95cbcbdf70870ca9583f
Brian Cong <brian.cong12345@gmail.com>
2023-08-02 20:57:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,17 @@ function getUniqueNamesInImageFolder() {
 
 getUniqueNamesInImageFolder();
 
+function initCards(card, index) {
+  var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+
+  newCards.forEach(function (card, index) {
+    card.style.zIndex = allCards.length - index;
+    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.opacity = (10 - index) / 10;
+  });
+  
+  tinderContainer.classList.add('loaded');
+}
 initCards();
 
 allCards.forEach(function (el) {
