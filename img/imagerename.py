4f42d2efa852df4263b29fbe2c7e11658fc28ce0
Brian Cong <brian.cong12345@gmail.com>
2023-08-03 12:33:09 -0400

    Major revision -- MVP
    still working out some kinks but swipe and button functionality work for all cards. currently dealing with a bug that causes list to regenerate too often after adding swipe functionality, hammer broke something, but works otherwise.

--- img/imagerename.py
+++ img/imagerename.py
@@ -9,15 +9,28 @@ import os
 import re
 
 def rename_images(directory='.'):
+    allnames = set()
     for filename in os.listdir(directory):
-        name, ext = os.path.splitext(filename)
-        match = re.match(r'(\D+)(\d+)$', name)
-        if match:
-            name_with_underscore = match.group(1) + '_' + match.group(2)
-            new_name = name_with_underscore + ext
-            os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
-            print(f"Renamed {filename} to {new_name}")
-
+        filename.replace('Cropped', '')
+        if not '_' in filename:
+            namearr = filename.split('.')
+            if 'jpg' in namearr or 'png' in namearr:
+                if not namearr[0].isalpha():
+                    finalname = namearr[0][0:len(namearr[0]) - 1] + '_' + namearr[0][-1] + '.jpg'
+                else:
+                    finalname = namearr[0] + '_1' + '.jpg'
+                #os.rename(filename, finalname)
+        else:
+            finalname = filename.replace("Cropped", '')
+            finalname = finalname.replace(".png", ".jpg")
+        print(finalname)
+        try:
+            os.rename(filename, finalname)
+        except:
+            print("Trying to rename" + filename + " to " + finalname)
+            continue
+        allnames.add(finalname.split('_')[0])
+    print(allnames)
 # Usage example:
 # Rename image files in the current directory
 rename_images()
--- script.js
+++ script.js
@@ -1,57 +1,24 @@
 'use strict';
 
 var tinderContainer = document.querySelector('.tinder');
+var cardsContainer = document.querySelector('.tinder--cards')
 var allCards = document.querySelectorAll('.tinder--card');
 var nope = document.getElementById('nope');
 var love = document.getElementById('love');
+var cardlength = 5
+var cardcount = 5
 const imgpath = './img/'
+const imgarray = ['DitzE', 'Barron', 'Beanie', 'Kym', 'Star', 'Skylocke', 
+                  'Deca', 'Pratreo', 'DejaWukong', 'Milkie', 'HeroAges', 'Chim', 
+                  'ChristFire', 'TripleJ', 'Scenecade', 'Skatey', 'Cocoa', 'Sol', 
+                  'Witchy', 'Sunji', 'Crowz', 'Nemu', 'Woney', 
+                  'Butler', 'Sylvie', 'Dragonspit', 'mlntchi', 'Jane', 'Mono', 
+                  'Veta', 'Zalion', 'Deadman', 'Endo', 'ChaosLily', 'Gemini', 
+                  'Mora', 'Kaia', 'Quattro', 'Xikesh', 'Ravenna', 'Kaze', 'Canaria', 
+                  'Goronyanya', 'Kayda', 'Oathborne', 'Macchi', 'Marble', 'Aldini', 
+                  'Riri', 'Mushashi', 'Xantherous', 'Draggon', 'Tigra', 'Apolloka', 'Saejima']
 
-// script.js
-function getUniqueNamesInImageFolder() {
-  const imagesFolderPath = './img/'; // Relative path to the 'img' subdirectory
-  const uniqueNames = [];
-
-  fetch(imagesFolderPath)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, 'text/html');
-      const imageElements = doc.querySelectorAll('a[href$=".jpg"]');
-
-      imageElements.forEach((image) => {
-        const nameWithoutSuffix = image.textContent.replace(/\d+\.\w+$/, ''); // Removes the number and extension
-        if (!uniqueNames.includes(nameWithoutSuffix)) {
-          uniqueNames.push(nameWithoutSuffix);
-        }
-      });
-
-      console.log('Unique names in the image folder:', uniqueNames);
-    })
-    .catch((err) => {
-      console.error('Error:', err);
-    });
-}
-
-getUniqueNamesInImageFolder();
-
-function initCards(card, index) {
-  var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
-
-  newCards.forEach(function (card, index) {
-    card.style.zIndex = allCards.length - index;
-    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
-    card.style.opacity = (10 - index) / 10;
-  });
-  
-  tinderContainer.classList.add('loaded');
-}
-initCards();
-
+imgarray.sort()
 allCards.forEach(function (el) {
   var hammertime = new Hammer(el);
 
@@ -99,6 +66,103 @@ allCards.forEach(function (el) {
     }
   });
 });
+
+function createCard(cardData, zIndex) {
+  const card = document.createElement('div');
+  card.classList.add('tinder--card');
+  card.style.zIndex = zIndex;
+  card.innerHTML = `
+    <img src="img/${cardData}_1.jpg">
+    <h3>${cardData}</h3>
+    <p>This is a demo for Tinder-like swipe cards</p>
+  `;
+  var hammertime = new Hammer(card);
+
+  hammertime.on('pan', function (event) {
+    card.classList.add('moving');
+  });
+
+  hammertime.on('pan', function (event) {
+    if (event.deltaX === 0) return;
+    if (event.center.x === 0 && event.center.y === 0) return;
+
+    tinderContainer.classList.toggle('tinder_love', event.deltaX > 0);
+    tinderContainer.classList.toggle('tinder_nope', event.deltaX < 0);
+
+    var xMulti = event.deltaX * 0.03;
+    var yMulti = event.deltaY / 80;
+    var rotate = xMulti * yMulti;
+
+    event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+  });
+
+  hammertime.on('panend', function (event) {
+    card.classList.remove('moving');
+    tinderContainer.classList.remove('tinder_love');
+    tinderContainer.classList.remove('tinder_nope');
+
+    var moveOutWidth = document.body.clientWidth;
+    var keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
+
+    event.target.classList.toggle('removed', !keep);
+
+    if (keep) {
+      event.target.style.transform = '';
+    } else {
+      var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
+      var toX = event.deltaX > 0 ? endX : -endX;
+      var endY = Math.abs(event.velocityY) * moveOutWidth;
+      var toY = event.deltaY > 0 ? endY : -endY;
+      var xMulti = event.deltaX * 0.03;
+      var yMulti = event.deltaY / 80;
+      var rotate = xMulti * yMulti;
+
+      event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+      initCards();
+    }
+  });
+
+  return card;
+}
+
+function initCards(card, index) {
+  var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+  var oldCards = document.querySelectorAll('.removed');
+  let cardImg = imgarray.shift()
+  let imgcopy = [... imgarray]
+  newCards.forEach(function (card, index) {
+    card.style.zIndex = allCards.length - index;
+    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.opacity = (10 - index) / 10;
+    console.log(cardImg)
+    card.innerHTML = `<img src="img/`+ cardImg +`_1.jpg" class="profile">
+    <h3>`+ cardImg + `</h3>
+    <p>This is a demo for Tinder like swipe cards</p>`
+    cardImg = imgcopy.shift()
+  });
+  console.log(newCards.length);
+  if(newCards.length < cardlength && imgcopy.length > 0){
+    //generate new cards
+    var newCard = createCard(cardImg + '', 5)
+    cardsContainer.appendChild(newCard)
+  }
+
+  tinderContainer.classList.add('loaded');
+}
+initCards();
+
+function updateCards(card, index){
+  var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+  var oldCards = document.querySelectorAll('.removed');
+  let cardImg = imgarray.shift()
+  let imgcopy = [... imgarray]
+  if(newCards.length < cardlength && imgcopy.length > 0){
+    //generate new cards
+    var newCard = createCard(cardImg + '', 5)
+    cardsContainer.appendChild(newCard)
+  }
+  tinderContainer.classList.add('loaded');
+}
 
 function createButtonListener(love) {
   return function (event) {
